4b5ddc14b03bf091d5cef760cccdbfcd18264abd
flo <f.imbert4@gmail.com>
2024-07-01 17:13:27 +0200

    LuxauNFT.test.js OK

--- test/LuxauNFT.test.js
+++ test/LuxauNFT.test.js
@@ -4,12 +4,18 @@ const {
 } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
 const { expect } = require("chai");
+const { ethers } = require('hardhat');
 
 describe("LuxauNFT", function () {
   async function deployContractFixture() {
-    const [owner, otherAccount] = await ethers.getSigners()
+    const _name = "MyToken";
+    const _symbol = "MTK";
+    const _baseURI = "ifps://base.uri/";
+
+    const [owner, otherAccount] = await ethers.getSigners();
     const LuxauNFT = await ethers.getContractFactory('LuxauNFT');
-    const luxauNFT = await LuxauNFT.deploy(LuxauNFT);
+    const luxauNFT = await LuxauNFT.deploy(_name, _symbol, _baseURI);
+
     return { luxauNFT, owner, otherAccount };
   }
 
@@ -17,6 +23,42 @@ describe("LuxauNFT", function () {
     it("Should deploy the smart contract", async function () {
       const { luxauNFT, owner } = await loadFixture(deployContractFixture);
       expect(await luxauNFT.owner()).to.equal(owner.address);
+      expect(await luxauNFT.name()).to.equal("MyToken");
+      expect(await luxauNFT.symbol()).to.equal("MTK");
+      expect(await luxauNFT.baseURI()).to.equal("ifps://base.uri/");
+    });
+  });
+
+  describe("MINT", function () {
+    describe("checks", function () {
+      it('Should NOT mint if not expected minimum price', async function () {
+        const { luxauNFT, owner } = await loadFixture(deployContractFixture);
+        await expect(luxauNFT.safeMint({ value: ethers.parseEther("0.001") })).to.be.revertedWith("Minimum price to mint is 0.01 ETH")
+      })
+    })
+    describe("effects", function () {
+      it('Should mint if expected minimum price', async function () {
+        const { luxauNFT, owner } = await loadFixture(deployContractFixture);
+        await expect(luxauNFT.safeMint({ value: ethers.parseEther("10") })).to.be.not.revertedWith("Minimum price to mint is 0.01 ETH")
+      })
     })
   })
+
+  describe("tokenURI", function () {
+    describe("checks", function () {
+      it('Should revert if token does not exist', async function () {
+        const { luxauNFT, owner } = await loadFixture(deployContractFixture);
+        expect(await luxauNFT.safeMint({ value: ethers.parseEther("0.01") }))
+        await expect(luxauNFT.tokenURI(1)).to.be.revertedWith("Token doesn't exist")
+      })
+      it('Should NOT revert if token exist', async function () {
+        const { luxauNFT, owner, otherAccount } = await loadFixture(deployContractFixture);
+        expect(await luxauNFT.safeMint({ value: ethers.parseEther("0.01") }))
+        expect(await luxauNFT.connect(otherAccount).safeMint({ value: ethers.parseEther("0.01") }))
+        await expect(luxauNFT.tokenURI(1)).to.be.not.revertedWith("Token doesn't exist")
+      })
+    })
+
+  })
+
 });

fc6def7b96129034a434d25254be5b34d89058e0
flo <f.imbert4@gmail.com>
2024-07-09 10:46:46 +0200

    hardhat.config.js updated

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,25 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
+require('dotenv').config()
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const INFURA_RPC_URL = process.env.INFURA_RPC_URL || "";
+const PK = process.env.PK || "";
+const ETHERSCAN = process.env.ETHERSCAN || "";
+
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: INFURA_RPC_URL,
+      accounts: [`0x${PK}`],
+      chainId: 11155111
+    },
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    }
+  },
+  etherscan: {
+    apiKey: ETHERSCAN
+  },
 };

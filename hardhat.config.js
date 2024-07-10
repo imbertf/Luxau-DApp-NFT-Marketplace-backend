ced89e9b5491a433da16b8e727c74eebc3be3750
flo <f.imbert4@gmail.com>
2024-07-10 14:51:33 +0200

    test baseSepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,20 +5,20 @@ require('dotenv').config()
 const ALCHEMY_RPC_URL = process.env.ALCHEMY_RPC_URL || "";
 const PK = process.env.PK || "";
 const ETHERSCAN = process.env.ETHERSCAN || "";
-console.log(PK);
+
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    // sepolia: {
-    //   url: ALCHEMY_RPC_URL,
-    //   accounts: [`0x${PK}`],
-    //   chainId: 11155111
-    // },
-    // base_sepolia: {
-    //   url: ALCHEMY_RPC_URL,
-    //   accounts: [`0x${PK}`],
-    //   chainId: 84532
-    // },
+    sepolia: {
+      url: ALCHEMY_RPC_URL,
+      accounts: [`0x${PK}`],
+      chainId: 11155111
+    },
+    base_sepolia: {
+      url: ALCHEMY_RPC_URL,
+      accounts: [`0x${PK}`],
+      chainId: 84532
+    },
     localhost: {
       url: "http://127.0.0.1:8545",
       chainId: 31337,

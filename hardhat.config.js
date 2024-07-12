1f4834fcddacecc08f21c13212b2d22e725bcda3
flo <f.imbert4@gmail.com>
2024-07-12 10:05:39 +0200

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -19,7 +19,7 @@ function utf8ToHex(str) {
 }
 
 const HARDHAT = utf8ToHex(PK_HARDHAT ?? '');
-const BASE_SEPOLIA = utf8ToHex(PK_BASE_SEPOLIA ?? '');
+const BASE_SEPOLIA = utf8ToHex(PK_BASE_SEPOLIA);
 
 module.exports = {
   solidity: "0.8.24",

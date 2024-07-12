bffb848f187d8c6b24b4954825c441b0988454c7
flo <f.imbert4@gmail.com>
2024-07-12 10:33:33 +0200

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,7 @@ require("@nomicfoundation/hardhat-verify");
 require('dotenv').config()
 
 const ALCHEMY_URL = process.env.ALCHEMY_RPC_URL || "";
-const PK = process.env.PK_BASE_SEPOLIA || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const PK = process.env.PK_BASE_SEPOLIA || "ac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 module.exports = {
   solidity: "0.8.24",

bd524ddf3911ce7de31371bbed60f56cede2d755
flo <f.imbert4@gmail.com>
2024-07-12 10:32:08 +0200

    test

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,14 +3,14 @@ require("@nomicfoundation/hardhat-verify");
 require('dotenv').config()
 
 const ALCHEMY_URL = process.env.ALCHEMY_RPC_URL || "";
-const PK = process.env.PK_BASE_SEPOLIA || "";
+const PK = process.env.PK_BASE_SEPOLIA || "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
 
 module.exports = {
   solidity: "0.8.24",
   networks: {
     base_sepolia: {
       url: ALCHEMY_URL,
-      accounts: [`${PK}`],
+      accounts: [`0x${PK}`],
       chainId: 84532
     },
     localhost: {
